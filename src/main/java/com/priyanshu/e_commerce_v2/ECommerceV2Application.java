49e431283aba2704bf9428fbc2e543bf37c198e3
pulsarchief <145131830+pulsarchief@users.noreply.github.com>
2025-08-31 18:39:25 +0530

    initial scaffolding set up and properties  and env draft

---
+++ src/main/java/com/priyanshu/e_commerce_v2/ECommerceV2Application.java
@@ -0,0 +1,13 @@
+package com.priyanshu.e_commerce_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshu/e_commerce_v2/ECommerceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.e_commerce_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
